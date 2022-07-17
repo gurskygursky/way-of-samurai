8130bdbe3dda720a6e34a885deb2e1a251d3b656
yegorgursky <yahorhurski@gmail.com>
2022-07-17 22:08:31 +0300

    install react router dom, use route and link

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Navbar} from './components/navbar/Navbar';
 import {Footer} from './components/footer/Footer';
 import {Profile} from './components/profile/Profile';
 import {Messages} from './components/messages/Messages';
+import {Route, Routes} from 'react-router-dom';
 
 
 export function App() {
@@ -14,8 +15,10 @@ export function App() {
             <Navbar/>
             <Footer/>
             <div className={"app-wrapper-content"}>
-                <Profile/>
-                {/*<Messages/>*/}
+                <Routes>
+                <Route path="/profile" element={<Profile />}/>
+                    <Route path={'/messages'} element={<Messages/>}/>
+                </Routes>
             </div>
         </div>
     );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <div className={'nav'}>
             <div style={{padding: '10px 0 0 10px'}}>
                 <div>
-                    <a>Profile</a>
+                    <Link to={'/profile'}>Profile</Link>
+                    {/*<a href={'/profile'}>Profile</a>*/}
                 </div>
                 <div>
-                    <a>News</a>
+                    {/*<a href={'/news'}>News</a>*/}
+                    <Link to={'/news'}>News</Link>
                 </div>
 
                 <div>
-                    <a>Messages</a>
+                    <Link to={'/messages'}>Messages</Link>
+                    {/*<a href={'/messages'}>Messages</a>*/}
                 </div>
                 <div>
-                    <a>Music</a>
+                    {/*<a href={'/music'}>Music</a>*/}
+                    <Link to={'/music'}>Music</Link>
                 </div>
                 <div>
-                    <a>Settings</a>
+                    <Link to={'/settings'}>Settings</Link>
+                    {/*<a href={'/settings'}>Settings</a>*/}
                 </div>
             </div>
         </div>
